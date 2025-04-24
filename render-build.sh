a7cfcbf33e33540694240167aa5308d303c4e13b
Proov33 <ezcroa@gmail.com>
2025-04-24 21:14:03 +0200

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
-# Mettre à jour les paquets du système
-apt-get update
+# Mettre à jour les paquets et installer les dépendances nécessaires
+apt-get update && apt-get install -y \
+  libnss3 \
+  libatk1.0-0 \
+  libatk-bridge2.0-0 \
+  libcups2 \
+  libxcomposite1 \
+  libxrandr2 \
+  libxdamage1 \
+  libgbm1 \
+  libasound2 \
+  fonts-liberation \
+  libappindicator3-1 \
+  libxshmfence1 \
+  libpangocairo-1.0-0 \
+  libx11-xcb1 \
+  libxshmfence-dev
 
-# Installer les dépendances nécessaires pour exécuter Playwright
-apt-get install -y wget curl unzip \
-    libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libxkbcommon0 \
-    libxcomposite1 libxrandr2 libxdamage1 libfontconfig1 libdrm2 \
-    libgbm1 libasound2 libpangocairo-1.0-0 libpangoft2-1.0-0
-
-# Installer Playwright avec les navigateurs nécessaires
-npx playwright install --with-deps
+# Installer les dépendances pour Puppeteer
+npm install
