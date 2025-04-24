3a41ec52e08fa671dc2ab402825b7cdb991ca09a
Proov33 <ezcroa@gmail.com>
2025-04-24 17:52:07 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { scrapeFlashscoreClub } = require('./scrapper');
+const { scrapeFlashscoreClub } = require('/scrapper');
 
 const app = express();
 const port = 3000;
